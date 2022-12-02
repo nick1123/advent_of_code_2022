aa7861e88586464fc9e46f5e6366141af6f50d4c
Nicholas Stewart <nicholas4@gmail.com>
2022-12-02 16:29:59 -0700

    Day 2: Rock, paper, scissors - part 2

---
+++ ruby/day_02/main_2.rb
@@ -0,0 +1,37 @@
+shape_selected_scoring = {'rock' => 1, 'paper' => 2, 'scissors' => 3}
+
+round_scoring = {
+  'rock'     => {'rock' => 3, 'paper' => 6, 'scissors' => 0},
+  'paper'    => {'rock' => 0, 'paper' => 3, 'scissors' => 6},
+  'scissors' => {'rock' => 6, 'paper' => 0, 'scissors' => 3}
+}
+
+translator_1 = {'A' => 'rock', 'B' => 'paper', 'C' => 'scissors'}
+
+translator_2 = {'X' => 'lose', 'Y' => 'draw', 'Z' => 'win'}
+
+translator_3 = {
+  'win'  => {'rock' => 'paper',    'paper' => 'scissors', 'scissors' => 'rock'},
+  'draw' => {'rock' => 'rock',     'paper' => 'paper',    'scissors' => 'scissors'},
+  'lose' => {'rock' => 'scissors', 'paper' => 'rock',     'scissors' => 'paper'},
+}
+
+sum = 0
+IO.readlines("input.txt").each do |line|
+  opponent, outcome = line.strip.split(/\s+/)
+  opponent = translator_1[opponent]
+  outcome = translator_2[outcome]
+  me = translator_3[outcome][opponent]
+  pp line
+  pp opponent
+  pp outcome
+  pp me
+  pp round_scoring[opponent][me]
+  pp shape_selected_scoring[me]
+  puts ''
+  sum += round_scoring[opponent][me] + shape_selected_scoring[me]
+end
+
+puts sum
+
+# 17549 is too high
