865108a097f96a75de6062dcc2ae9ece5f830804
Nicholas Stewart <nicholas4@gmail.com>
2022-12-05 17:15:45 -0700

    Day 5: Part 1 done

--- ruby/day_05/main.rb
+++ ruby/day_05/main.rb
@@ -2,7 +2,6 @@ def read_input
   IO.read("input.txt")
 end
 
-
 def alpha?(char)
   char.ord >= 65 && char.ord <= 90
 end
@@ -11,9 +10,9 @@ def parse_stack_number(index)
   (index + 3) / 4
 end
 
+# Ex: {2=>["D", "C", "M"], 1=>["N", "Z"], 3=>["P"]}
 def parse_diagram
   lines = read_input.split("\n\n")[0].split("\n")
-  puts lines
 
   # Remove bottom line `1   2   3 `
   lines.pop
@@ -23,8 +22,8 @@ def parse_diagram
   lines.each do |line|
     line.split("").each_with_index do |char, index|
       next unless alpha?(char)
-      stack_number = parse_stack_number(index)
 
+      stack_number = parse_stack_number(index)
       stacks[stack_number] ||= []
       stacks[stack_number] << char
     end
@@ -33,15 +32,33 @@ def parse_diagram
   stacks
 end
 
+# Ex: [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]]
 def parse_instructions
-  read_input.split("\n\n")[1].split("\n")
+  read_input.split("\n\n")[1].split("\n").map do |line|
+    chunks = line.strip.split(/\s+/)
+    [chunks[1], chunks[3], chunks[5]].map(&:to_i)
+  end
+end
+
+def run(stacks, instructions)
+  instructions.each do |iterations, from_stack, to_stack|
+    iterations.times do
+      stacks[to_stack].unshift(stacks[from_stack].shift)
+    end
+  end
+end
+
+def top_of_each_stack(stacks)
+  stacks.keys.sort.map do |index|
+    stacks[index][0]
+  end.join
 end
 
 def part_1
   instructions = parse_instructions
   stacks = parse_diagram
-  pp stacks
-  # pp instructions
+  run(stacks, instructions)
+  puts top_of_each_stack(stacks)
 end
 
 part_1
